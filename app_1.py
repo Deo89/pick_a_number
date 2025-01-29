fcb1c85092513e8949b4fa90491ab86167008165
Deo89 <deoxt.p@gmail.com>
2025-01-29 14:19:41 +0200

    Add files via upload
    Picks a slider number and dispalys it

---
+++ app_1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 10:50:49 2025
+
+@author: deoxt
+"""
+import streamlit as st
+
+st.title("Never use spaces in files and folder names!!")
+st.title("Hello world")
+
+st.title("dont come")
+st.write("this is my first name")
+
+number  = st.slider ("Pick a number", 1, 100)
+
+st.write(f"you picked {number}")
